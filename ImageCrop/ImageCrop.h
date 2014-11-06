20ed05010c25e83ea978fbb538c18734e7b94554
Yuma Oda <yuma.oda@eure.jp>
2014-11-06 18:10:39 +0900

    Initial Commit

---
+++ ImageCrop/ImageCrop.h
@@ -0,0 +1,19 @@
+//
+//  ImageCrop.h
+//  ImageCrop
+//
+//  Created by yumaoda on 2014/11/06.
+//  Copyright (c) 2014年 yumaoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageCrop.
+FOUNDATION_EXPORT double ImageCropVersionNumber;
+
+//! Project version string for ImageCrop.
+FOUNDATION_EXPORT const unsigned char ImageCropVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageCrop/PublicHeader.h>
+
+
