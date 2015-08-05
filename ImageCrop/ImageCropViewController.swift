8ab3208437fde125b0c9c016e6623c9c6b338799
muukii <muukii.muukii@gmail.com>
2015-08-05 18:48:02 +0900

    wip :D

--- ImageCrop/ImageCropViewController.swift
+++ ImageCrop/ImageCropViewController.swift
@@ -413,7 +413,7 @@ class ImageCropViewController: UIViewController {
             self.configureView()
         }
         
-        required init(coder aDecoder: NSCoder) {
+        required init?(coder aDecoder: NSCoder) {
             super.init(coder: aDecoder)
             self.configureView()
         }
